1f04ab41de35849853c7704cc614f6a6561405e4
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2015-10-14 21:44:24 +0100

    Building website

--- content/infrastructure/install_gds_stack_unix.sh
+++ content/infrastructure/install_gds_stack_unix.sh
@@ -24,20 +24,27 @@ source activate gds
 
 conda install -n gds -y jupyter
 
-conda install -n gds -y pandas scipy matplotlib seaborn statsmodels scikit-learn
+conda install -n gds -y pandas=0.16 scipy matplotlib seaborn statsmodels scikit-learn
 
 pip install -U pysal
 pip install -U clusterpy
 
-conda install -n gds -y fiona six
+conda install -n gds -y fiona=1.5 six
 conda install -n gds psycopg2
 conda install -n gds -y shapely
 conda install -n gds -y pyproj
 pip install -U geopy
 pip install -U descartes
-pip install --no-deps geopandas
+pip install --no-deps git+git://github.com/geopandas/geopandas.git
 
 # Switch geos version to install cartopy correctly
 conda install -n gds -y -c scitools geos
 conda install -n gds -y -c scitools cartopy
+
+# Additional
+conda install -n gds -y rasterio=0.24
+pip install -U Jinja2
+pip install -U mplleaflet
+conda install -n gds -y pytables=3.2.1
+
 
